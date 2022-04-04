f4b234f7f469182d752d6d903eb4396d61739fb1
LontchiFrank <fodjof6@gmail.com>
2022-04-04 21:42:03 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-// import React from "react";
+// // import React from "react";
+
+// // function App() {
+// //   return <div>App</div>;
+// // }
+
+// // export default App;
+
+// import "./App.css";
+// import Navigation from "./Navigation";
+// // import "bootstrap/dist/css/bootstrap.min.css";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
 
 // function App() {
-//   return <div>App</div>;
+//   return (
+//     <div className="App">
+//       <Provider store={store}>
+//         <h2>Hello </h2>
+//         {/* <Navigation /> */}
+//       </Provider>
+//       {/* <h2>Hello World</h2> */}
+//     </div>
+//   );
 // }
 
 // export default App;
 
-import "./App.css";
-import Navigation from "./Navigation";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <h2>Hello </h2>
-        {/* <Navigation /> */}
-      </Provider>
-      {/* <h2>Hello World</h2> */}
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
