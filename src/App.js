5d10000932643765b0ca633d71f36bf93dbedb61
LontchiFrank <fodjof6@gmail.com>
2022-04-04 19:20:04 +0100

    so we thank GOD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+// import React from "react";
+
+// function App() {
+//   return <div>App</div>;
+// }
+
+// export default App;
+
+import "./App.css";
+import Navigation from "./Navigation";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <h2>Hello World</h2>
+        {/* <Navigation /> */}
+      </Provider>
+      {/* <h2>Hello World</h2> */}
+    </div>
+  );
 }
 
 export default App;
