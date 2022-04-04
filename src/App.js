f656aa9bbb28ca41c3e1070535508559eb7d3a39
LontchiFrank <fodjof6@gmail.com>
2022-04-04 18:25:54 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import "./App.css";
-import Navigation from "./Navigation";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Provider store={store}>
-        <Navigation />
-      </Provider> */}
-      <h2>Hello World</h2>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
-
-import React from "react";
